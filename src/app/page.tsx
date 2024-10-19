58392169c995c84c90dd8b5229e22ce9258ec0c9
NughbaIslam-48 <nughbai@gmail.com>
2024-10-19 01:12:21 -0700

    contact-form

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+// import Layout from "@/component/Layout/layout.jsx";
+import Topbar from '../Component/Layout/Topbar'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Topbar/>
+      
+    </div>   
+  );
+}
+  
